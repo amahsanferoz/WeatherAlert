c153ca050b857a33e35f48e7ce61a7e7c1182bb3
Ahsan Feroz <feroz@jongla.com>
2016-06-17 15:25:26 +0300

    Project created as a single page application

---
+++ Weather Alert/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather Alert
+//
+//  Created by Ahsan Feroz on 17/06/16.
+//  Copyright © 2016 Ahsan Feroz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
